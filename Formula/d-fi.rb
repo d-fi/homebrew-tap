08260464d04de68f10f3014c314eae0497f504b5
Sayem Chowdhury <sayem314@gmail.com>
2026-05-27 01:52:56 +0600

    Add d-fi formula

---
+++ Formula/d-fi.rb
@@ -0,0 +1,17 @@
+class DFi < Formula
+  desc "Deezer downloader CLI and local web UI"
+  homepage "https://github.com/d-fi/GoFi"
+  url "https://github.com/d-fi/releases/releases/download/2.2.0/d-fi-macos.zip"
+  sha256 "27e0671c2f550c25b446865b8be60a769d5d3daea6cea822ea295135e4450342"
+  version "2.2.0"
+  license "MIT"
+
+  def install
+    chmod 0755, "d-fi"
+    bin.install "d-fi"
+  end
+
+  test do
+    assert_match "Usage of d-fi", shell_output("#{bin}/d-fi --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,23 @@
+# Homebrew Tap for d-fi
+
+This tap installs the prebuilt macOS binary for d-fi.
+
+## Install
+
+```sh
+brew tap d-fi/tap
+brew install d-fi
+```
+
+## Usage
+
+```sh
+d-fi --help
+```
+
+## Formula
+
+- Formula: `Formula/d-fi.rb`
+- Version: `2.2.0`
+- Source project: <https://github.com/d-fi/GoFi>
+- Binary release: <https://github.com/d-fi/releases/releases/tag/2.2.0>
