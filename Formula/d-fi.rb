11ccc442c479bbe06006e3a388dffdc18dd1c3cb
Sayem Chowdhury <sayem314@gmail.com>
2026-06-01 00:49:10 +0600

    Update d-fi to 2.3.2

--- Formula/d-fi.rb
+++ Formula/d-fi.rb
@@ -1,8 +1,8 @@
 class DFi < Formula
   desc "Deezer downloader CLI and local web UI"
   homepage "https://github.com/d-fi/GoFi"
-  url "https://github.com/d-fi/GoFi/archive/refs/tags/v2.3.0.tar.gz"
-  sha256 "24986fa9afc18447d862d85d73fbfdcbe3c5957a595a5f08e5dba898cb8a641b"
+  url "https://github.com/d-fi/GoFi/archive/refs/tags/v2.3.2.tar.gz"
+  sha256 "0f8f07aed7b3845ef15c7b5f6cb7bb3bd2e43d33b8f4bc2658d4d9ac535a6228"
   license "MIT"
 
   depends_on "go" => :build
--- README.md
+++ README.md
@@ -18,6 +18,6 @@ d-fi --help
 ## Formula
 
 - Formula: `Formula/d-fi.rb`
-- Version: `2.3.0`
+- Version: `2.3.2`
 - Source project: <https://github.com/d-fi/GoFi>
-- Source tag: <https://github.com/d-fi/GoFi/releases/tag/v2.3.0>
+- Source tag: <https://github.com/d-fi/GoFi/releases/tag/v2.3.2>
